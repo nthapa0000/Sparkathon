4cc75f066adece38aa3130a12983393fae7da498
nthapa0000 <21bma029@nith.ac.in>
2023-08-17 18:08:51 +0530

    Now will add shadcn/ui for forms

--- Sparkathon-Homepage/app/(auth)/onboarding/page.tsx
+++ Sparkathon-Homepage/app/(auth)/onboarding/page.tsx
@@ -1,6 +1,25 @@
+import AccountProfile from '@/components/forms/AccountProfile'
+import { currentUser } from '@clerk/nextjs'
 import React from 'react'
 
+
 async function Page() {
+  const user = await currentUser();
+  
+  const userInfo = {};
+
+  // question mark to confirm it exist
+  const userData = {
+    id: user?.id,
+    objectId: userInfo?._id,
+    username: userInfo?.username || user?.username,
+    name: userInfo?.name || user?.firstName || "",
+    bio: userInfo?.bio || "",
+    image: userInfo?.image || user?.imageUrl,
+
+
+  }
+
   return (
     <main className='mx-auto flex max-w-3xl flex-col justify-start px-10 py-20'>
       <h1 className='text-heading2-bold'>Onboarding</h1>
@@ -8,7 +27,9 @@ async function Page() {
         Complete your profile to use WalmartUnity
       </p>
       <section className='mt-9 bg-slate-300 p-10'>
-
+        <AccountProfile user={userData} 
+        btnTitle="Continue" 
+      />
       </section>
     </main>
   )
---
+++ Sparkathon-Homepage/components/forms/AccountProfile.tsx
@@ -0,0 +1,27 @@
+"use client"
+
+interface Props {
+    user:{
+        id:string,
+        objectId:string,
+        username:string,
+        name:string,
+        bio:string,
+        image:string
+    }
+    btnTitle:string
+}
+
+const AccountProfile = ({
+    user,btnTitle
+}:Props) => {
+    return (
+        <div>
+            Account Profile
+        </div>
+    )
+}
+
+export default AccountProfile
+
+// we will pass data of current user
