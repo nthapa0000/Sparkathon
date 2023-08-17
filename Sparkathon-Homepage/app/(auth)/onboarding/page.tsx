8f037ee5cdb971d567608273324de22755c47ecf
nthapa0000 <21bma029@nith.ac.in>
2023-08-17 17:57:12 +0530

    Onboarding page after sign in

---
+++ Sparkathon-Homepage/app/(auth)/onboarding/page.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+async function Page() {
+  return (
+    <main className='mx-auto flex max-w-3xl flex-col justify-start px-10 py-20'>
+      <h1 className='text-heading2-bold'>Onboarding</h1>
+      <p className='mt-3 text-base-regular '>
+        Complete your profile to use WalmartUnity
+      </p>
+      <section className='mt-9 bg-slate-300 p-10'>
+
+      </section>
+    </main>
+  )
+}
+
+export default Page
